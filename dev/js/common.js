a53392e911ff839d21722ca048c4ba632444b0f0
Максим Cтуденикин <operatoribm@icloud.com>
2018-02-25 18:44:43 +0300

    последние наработки

--- dev/js/common.js
+++ dev/js/common.js
@@ -30,6 +30,55 @@ $(document).ready(function() {
     popUpComponents('.open__form', '.form__feedback', '.close__batton-feedback')
     popUpComponents('.h__menu-link', '.h__menu', '.h__menu-close__batton')
 
+    //slider
+
+    const moveSlide = (cont, slideNum) => {
+
+        const items = cont.find('.slaider__item'),
+            activeSlide = items.filter('.active'),
+            reqItem = items.eq(slideNum),
+            reqIndex = reqItem.index(),
+            list = cont.find('.slaider__list'),
+            dur = 500;
+
+        if (reqItem.length) {
+            list.animate({
+                'left': -reqIndex * 100 + '%'
+            }, dur, () => {
+                activeSlide.removeClass('active');
+                reqItem.addClass('active');
+            });
+        }
+
+    };
+
+    $('.slider__controls').on('click', event => {
+        event.preventDefault();
+
+        const $this = $(event.currentTarget),
+            cont = $this.closest('.slaider__teg'),
+            items = $('.slaider__item', cont),
+            activeItem = items.filter('.active');
+        let existedItem,
+            edgeItem,
+            reqItem;
+
+        if ($this.hasClass('controls__btn__next')) {
+            existedItem = activeItem.next();
+            edgeItem = items.first();
+        }
+
+        if ($this.hasClass('controls__btn__prev')) {
+            existedItem = activeItem.prev();
+            edgeItem = items.last();
+        }
+
+        reqItem = existedItem.length ? existedItem.index() : edgeItem.index();
+
+        moveSlide(cont, reqItem);
+
+    });
+
    
     //Галерея
 
