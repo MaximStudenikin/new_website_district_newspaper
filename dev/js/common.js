ac450873b95a2bc75756d67cfca2c34ee7b2cf83
Maxim Studenikin <operatoribm@gmail.com>
2018-03-02 21:07:56 +0300

    просто лень кидать на комп

--- dev/js/common.js
+++ dev/js/common.js
@@ -30,14 +30,72 @@ $(document).ready(function(){
     popUpComponents('.open__form', '.form__feedback', '.close__batton-feedback')
     popUpComponents('.h__menu-link', '.h__menu', '.h__menu-close__batton')
 
-    //slider 
+    //slider
+
+     //generation number of slide
+
+     var generatePagination = () => {
+
+        var slidrs = $('.slider__teg'),
+            list = slidrs.find('.pagination__list');
+
+        console.log()
+
+                $('.slider__item').each(()=>{
+                    let paginationItem = $('<li>',{
+                        attr : {
+                            class: 'pagination__item'
+                        },
+                        html : '<a class="pagination__link">0</a>'
+                    });
+                    
+                    list.append(paginationItem);
+                })
+            
+    };
+
+    generatePagination();
+
+       //color active pagination slide
+
+       var activeSlide = (index)=>{
+        $('.slider__teg')
+            .find('.pagination__item')
+            .eq(index)
+            .addClass('.pagination__link--activ')
+            .siblings()
+            .removeClass('.pagination__link--activ');
+    }
+
+       //listing slid
+
+       const moveSlide = (cont, slideNum) => {
+
+        const items = cont.find('.slider__item'),
+            activeSlide = items.filter('.active__slide'),
+            reqItem = items.eq(slideNum),
+            reqIndex = reqItem.index(),
+            list = cont.find('.slider__list'),
+            dur = 500;
+
+        if (reqItem.length) {
+            list.animate({
+                'left': -reqIndex * 100 + '%'
+            }, dur, () => {
+                activeSlide.removeClass('active__slide');
+                reqItem.addClass('active__slide');
+                // activeSlide(slideNum);
+            });
+        }
+
+    };
 
     //click ruls btn
 
     $('.slider__controls').on('click', event => {
         event.preventDefault();
 
-        const $this = $( event.target),
+        const $this = $(event.target),
             cont = $this.closest('.slider__teg'),
             items = $('.slider__item', cont),
             activeItem = items.filter('.active__slide');
@@ -61,54 +119,6 @@ $(document).ready(function(){
 
     });
 
-        //listing slid
-
-        const moveSlide = (cont, slideNum) => {
-
-            const items = cont.find('.slider__item'),
-                activeSlide = items.filter('.active__slide'),
-                reqItem = items.eq(slideNum),
-                reqIndex = reqItem.index(),
-                list = cont.find('.slider__list'),
-                dur = 500;
-    
-            if (reqItem.length) {
-                list.animate({
-                    'left': -reqIndex * 100 + '%'
-                }, dur, () => {
-                    activeSlide.removeClass('active__slide');
-                    reqItem.addClass('active__slide');
-                    // activeSlide(slideNum);
-                });
-            }
-            
-    
-        };
-
-     //generation number of slide
-
-     var generatePagination = () => {
-
-        const sliders = $('.slider__teg'),
-            reqslider = sliders.eq(sliderNum),
-            reqIndex = reqslider.index(sliderIndex);
-
-
-                $('.slider__item').each(() =>{
-                    let paginationItem = $('<li>',{
-                        attr : {
-                            class: 'pagination__item'
-                        },
-                        html : '<a class="pagination__link">0</a>'
-                    });
-                    
-                    $('.pagination__list').eq(sliderIndex).append(paginationItem);
-                })
-            
-    };
-
-    generatePagination();
-
     //click on pugination
 
    $('body').on('click', '.pagination__item', event => {
@@ -122,17 +132,6 @@ $(document).ready(function(){
     activeSlide(index);
    });
 
-    //color active slide
-
-    var activeSlide = (index)=>{
-        $('.slider__teg')
-        .find('.pagination__item')
-        .eq(index)
-        .addClass('.pagination__link--activ')
-        .siblings()
-        .removeClass('.pagination__link--activ');
-    }
-   
     //Галерея
 
     $('[data-fancybox]').fancybox({
