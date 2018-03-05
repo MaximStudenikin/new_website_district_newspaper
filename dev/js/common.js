c3303405ee8e3f4b64e79f92c5404b75121fde9b
Maxim Studenikin <operatoribm@gmail.com>
2018-03-05 14:29:10 +0300

    функция слайдер начало

--- dev/js/common.js
+++ dev/js/common.js
@@ -31,41 +31,9 @@ $(document).ready(function () {
     popUpComponents('.h__menu-link', '.h__menu', '.h__menu-close__batton')
 
     //slider
-
-    var colorAtiveItem = function (container, index) {
-
-            $('.pagination__item',container)
-            .eq(index)
-            .addClass('pagination__item--activ')
-            .siblings()
-            .removeClass('pagination__item--activ');
-    }
-
-    //generation number of slide
-
-    var generateItems = function () {
-
-        var sliders = $('.slider__teg');
-
-        for (var indexSlider = 0; indexSlider < sliders.length; indexSlider++) {
-            currentSliders = sliders[indexSlider];
-
-            $('.slider__item', currentSliders).each(function (indexNumber) {
-
-                var paginationItem = $('<li>', {
-                    attr: {
-                        class: 'pagination__item'
-                    },
-                    html: '<a class="pagination__link">' + (indexNumber + 1) + '</a>'
-                });
-
-                $('.pagination__list', currentSliders).append(paginationItem);
-            })
-        }
-    };
-
-    generateItems();
-
+    var slider_def = function (sliderContainer, sliderList, sliderItem, activeSlide, sliderControlBtn, 
+                                btnPrev, btnNext, paginationList, pagItem, pagItemActiv) {
+    
     //searh number slid and activ slid 
 
     var moveSlide = function (cont, slideNum) {
@@ -83,7 +51,7 @@ $(document).ready(function () {
             }, dur, function () {
                 activeSlide.removeClass('active__slide');
                 reqItem.addClass('active__slide');
-                colorAtiveItem(slideNum);
+                colorAtiveItem(cont, slideNum);
             });
         }
 
@@ -118,6 +86,40 @@ $(document).ready(function () {
 
     });
 
+    //generation number of slide
+
+    var generateItems = function () {
+
+        var sliders = $('.slider__teg');
+
+        for (var indexSlider = 0; indexSlider < sliders.length; indexSlider++) {
+            currentSliders = sliders[indexSlider];
+
+            $('.slider__item', currentSliders).each(function (indexNumber) {
+
+                var paginationItem = $('<li>', {
+                    attr: {
+                        class: 'pagination__item'
+                    },
+                    html: '<a class="pagination__link">' + (indexNumber + 1) + '</a>'
+                });
+
+                $('.pagination__list', currentSliders).append(paginationItem);
+            })
+        }
+    }();
+
+    // color active item pagination
+
+    var colorAtiveItem = function (container, index) {
+
+        $('.pagination__item', container)
+            .eq(index)
+            .addClass('pagination__item--activ')
+            .siblings()
+            .removeClass('pagination__item--activ');
+    }
+
     //click on pugination
 
     $('body').on('click', '.pagination__item', function () {
@@ -130,7 +132,8 @@ $(document).ready(function () {
         colorAtiveItem(container, index);
     });
 
-    //Галерея
+}();
+    //gallery
 
     $('[data-fancybox]').fancybox({
         toolbar: true
