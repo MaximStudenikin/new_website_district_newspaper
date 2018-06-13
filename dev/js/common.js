73a7b42b4722172799b5b9072b87da1920953b3e
Maxim Studenikin <operatoribm@gmail.com>
2018-06-13 13:49:59 +0300

    update common.js

--- dev/js/common.js
+++ dev/js/common.js
@@ -1,279 +1,349 @@
 $(document).ready(function () {
 
-    //app global var zon
+//app global var zon
 
-    var doc = document;
-    var mobailDetect = new MobileDetect(window.navigator.userAgent);
-    var isMobail = mobailDetect.mobile();
+var doc = document;
+var mobailDetect = new MobileDetect(window.navigator.userAgent);
+var isMobail = mobailDetect.mobile();
 
-    //Открыть элементы
-    var popUpComponents = (buttonToShow, needComponents, buttonToHide) => {
+//Открыть элементы
+var popUpComponents = (buttonToShow, needComponents, buttonToHide) => {
 
-        $(buttonToShow).click(function (buttonToShow) {
+    $(buttonToShow).click(function (buttonToShow) {
 
-            buttonToShow.preventDefault();
+        buttonToShow.preventDefault();
 
-            $(needComponents).toggleClass('visuallyhidden');
+        $(needComponents).toggleClass('visuallyhidden');
 
+    })
+
+    $(buttonToHide).click(function (buttonToHide) {
+
+        buttonToHide.preventDefault();
+
+        $(needComponents).toggleClass('visuallyhidden');
+
+    })
+}
+
+popUpComponents('.open__form', '.form-feedback', '.close__batton-feedback')
+popUpComponents('.h__menu-link', '.h__menu', '.h__menu-close__batton')
+popUpComponents('.categories--open', '.categories', '.categories--close')
+
+//vertical accordion
+
+$('.categories__trigger').on('click touchstart', event => {
+    event.preventDefault();
+
+    var $this = $(event.currentTarget);
+    var container = $this.closest('.categories-accordeon');
+    var item = $this.closest('.categories__item');
+    var items = $('.categories__item', container);
+    var content = $('.categories__content', item);
+    var otherContent = $('.categories__content', container);
+    var block = $('.categories__content_inner', item);
+    var reqHeight = block.outerHeight();
+
+    if (!item.hasClass('categories__item--activ')) {
+        items.removeClass('categories__item--activ')
+        item.addClass('categories__item--activ')
+
+        otherContent.css({
+            'height': 0
         })
 
-        $(buttonToHide).click(function (buttonToHide) {
+        content.css({
+            'height': reqHeight
+        })
 
-            buttonToHide.preventDefault();
+    } else {
 
-            $(needComponents).toggleClass('visuallyhidden');
-
+        item.removeClass('categories__item--activ');
+        content.css({
+            'height': 0
         })
     }
-    
-    popUpComponents('.open__form', '.form__feedback', '.close__batton-feedback')
-    popUpComponents('.h__menu-link', '.h__menu', '.h__menu-close__batton')
-    popUpComponents('.categories--open', '.categories', '.categories--close')
-
-    //vertical accordion
-
-    $('.categories__trigger').on('click touchstart', event => {
-        event.preventDefault();
-
-        const $this = $(event.currentTarget);
-        const container = $this.closest('.categories-accordeon');
-        const item = $this.closest('.categories__item');
-        const items = $('.categories__item', container);
-        const content = $('.categories__content', item);
-        const otherContent = $('.categories__content', container);
-        const block = $('.categories__content_inner', item);
-        const reqHeight = block.outerHeight();
-
-        if (!item.hasClass('categories__item--activ')) {
-            items.removeClass('categories__item--activ')
-            item.addClass('categories__item--activ')
-
-            otherContent.css({
-                'height': 0
-            })
-
-            content.css({
-                'height': reqHeight
-            })
-
-        } else {
-
-            item.removeClass('categories__item--activ');
-            content.css({
-                'height': 0
-            })
-        }
 
 })
 
-    //slider
+//slider
 
-    //click ruls btn
+//click ruls btn
 
-    $('[data-class="slider__controls"]').on('click touchstart', function (event) {
-        event.preventDefault();
+$('[data-class="slider__controls"]').on('click touchstart', function (event) {
+    event.preventDefault();
 
-        var $this = $(event.target),
-            cont = $this.closest('[data-class="slider__teg"]'),
-            items = $('[data-class="slider__item"]', cont),
-            activeItem = items.filter('.active__slide');
-        var existedItem,
-            edgeItem,
-            reqItem;
+    var $this = $(event.target),
+        cont = $this.closest('[data-class="slider__teg"]'),
+        items = $('[data-class="slider__item"]', cont),
+        activeItem = items.filter('.active__slide');
+    var existedItem,
+        edgeItem,
+        reqItem;
 
-        if ($this.hasClass('controls__btn_next')) {
-            existedItem = activeItem.next();
-            edgeItem = items.first();
-        }
-
-        if ($this.hasClass('controls__btn_prev')) {
-            existedItem = activeItem.prev();
-            edgeItem = items.last();
-        }
-        
-        reqItem = existedItem.length ? existedItem.index() : edgeItem.index();
-
-        moveSlide(cont, reqItem);  
-
-    });
-
-
-    //searh number slid and activ slide
-
-    var moveSlide = function (cont, slideNum) {
-
-        var items = cont.find('[data-class="slider__item"]'),
-            activeSlide = items.filter('.active__slide'),
-            reqItem = items.eq(slideNum),
-            reqIndex = reqItem.index(),
-            list = cont.find('[data-class="slider__list"]'),
-            dur = 500;
-
-        if (reqItem.length) {
-            list.animate({
-                'left': -reqIndex * 100 + '%'
-            }, dur, function () {
-                activeSlide.removeClass('active__slide');
-                reqItem.addClass('active__slide');
-                colorAtiveItem(cont, slideNum);
-            });
-    };
-
-    };
-
-    //generation number of slide
-
-    var generateItems = function (cont, sliderItem, paginationList) {
-
-        var sliders = $(cont);
-
-        for (var indexSlider = 0; indexSlider < sliders.length; indexSlider++) {
-            currentSliders = sliders[indexSlider];
-
-            $(sliderItem, currentSliders).each(function (indexNumber) {
-
-                var paginationItem = $('<li>', {
-                    attr: {
-                        class: 'pagination__item'
-                    },
-                    html: '<a class="pagination__link">' + (indexNumber + 1) + '</a>'
-                });
-
-                $(paginationList, currentSliders).append(paginationItem);
-            })
-        }
-    };
-
-    generateItems('[data-class="slider__teg"]', '[data-class="slider__item"]', '[data-class="pagination__list"]');
-
-    // color active item pagination
-
-    var colorAtiveItem = function (container, index) {
-
-        $('.pagination__item', container)
-            .eq(index)
-            .addClass('pagination__item--activ')
-            .siblings()
-            .removeClass('pagination__item--activ');
+    if ($this.hasClass('controls__btn_next')) {
+        existedItem = activeItem.next();
+        edgeItem = items.first();
     }
 
-    //click on pugination slider
+    if ($this.hasClass('controls__btn_prev')) {
+        existedItem = activeItem.prev();
+        edgeItem = items.last();
+    }
 
-    $('body').on('click touchstart', '.pagination__item', function () {
+    reqItem = existedItem.length ? existedItem.index() : edgeItem.index();
 
-        var $this = $(this),
-            container = $this.closest('[data-class="slider__teg"]'),
-            index = $this.index();
+    moveSlide(cont, reqItem);
 
+});
+
+
+//searh number slid and activ slide
+
+var moveSlide = function (cont, slideNum) {
+
+    var items = cont.find('[data-class="slider__item"]'),
+        activeSlide = items.filter('.active__slide'),
+        reqItem = items.eq(slideNum),
+        reqIndex = reqItem.index(),
+        list = cont.find('[data-class="slider__list"]'),
+        dur = 500;
+
+    if (reqItem.length) {
+        list.animate({
+            'left': -reqIndex * 100 + '%'
+        }, dur, function () {
+            activeSlide.removeClass('active__slide');
+            reqItem.addClass('active__slide');
+            colorAtiveItem(cont, slideNum);
+        });
+    };
+
+};
+
+//generation number of slide
+
+var generateItems = function (cont, sliderItem, paginationList) {
+
+    var sliders = $(cont);
+
+    for (var indexSlider = 0; indexSlider < sliders.length; indexSlider++) {
+        currentSliders = sliders[indexSlider];
+
+        $(sliderItem, currentSliders).each(function (indexNumber) {
+
+            var paginationItem = $('<li>', {
+                attr: {
+                    class: 'pagination__item'
+                },
+                html: '<a class="pagination__link">' + (indexNumber + 1) + '</a>'
+            });
+
+            $(paginationList, currentSliders).append(paginationItem);
+        })
+    }
+};
+
+generateItems('[data-class="slider__teg"]', '[data-class="slider__item"]', '[data-class="pagination__list"]');
+
+// color active item pagination
+
+var colorAtiveItem = function (container, index) {
+
+    $('.pagination__item', container)
+        .eq(index)
+        .addClass('pagination__item--activ')
+        .siblings()
+        .removeClass('pagination__item--activ');
+}
+
+//click on pugination slider
+
+$('body').on('click touchstart', '.pagination__item', function () {
+
+    var $this = $(this),
+        container = $this.closest('[data-class="slider__teg"]'),
+        index = $this.index();
+
+    moveSlide(container, index);
+    colorAtiveItem(container, index);
+
+
+});
+
+$(window).swipe({
+    swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
         moveSlide(container, index);
         colorAtiveItem(container, index);
+    }
+})
 
+//slidershow
 
-    });
+//slidershow controls
 
-    $(window).swipe({
-        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
-            moveSlide(container, index);
-        colorAtiveItem(container, index);
-        }
-    })
+$('.slidershow__controls').on('click touchstart', function (event) {
+    event.preventDefault();
 
-    //slidershow
+    var $this = $(event.target),
+        container = $this.closest('.slidershow'),
+        items = $('.slidershow__item', container),
+        activeItem = items.filter('.slidershow__item--activ'),
+        browsingReview = $('.slidershow__review', container),
+        browsingLink = $('.browsing__link', container),
+        preloader = $('.slidershow__preloader', container),
+        existedItem,
+        edgeItem,
+        reqItems;
 
-    //slidershow controls
-
-    $('.slidershow__controls').on('click touchstart', function (event) {
-        event.preventDefault();
-
-        var $this = $(event.target),
-            container = $this.closest('.slidershow'),
-            items = $('.slidershow__item', container),
-            activeItem = items.filter('.slidershow__item--activ'),
-            browsingReview = $('.slidershow__review', container),
-            browsingLink = $('.browsing__link', container),
-            preloader = $('.slidershow__preloader', container),
-            existedItem,
-            edgeItem,
-            reqItems;
-
-        if ($this.hasClass('controls__btn_next')) {
-            existedItem = activeItem.next();
-            existedItem.addClass('slidershow__item--activ');
-            existedItem.siblings().removeClass('slidershow__item--activ');
-            nextPath = $('.slidershow__link', '.slidershow__item--activ').attr('href');
-
-            browsingReview.fadeOut(function () {
-                preloader.show();
-                browsingReview.attr('src', nextPath).on('load', function () {
-                    browsingLink.attr('href', nextPath);
-                    browsingReview.fadeIn();
-                    preloader.hide();
-                })
-            })
-            var nextIndex = existedItem.index();
-        }
-        if (nextIndex == -1) {
-            edgeItem = items.first();
-            edgeItem.addClass('slidershow__item--activ');
-            edgeItem.siblings().removeClass('slidershow__item--activ');
-            nextPath = $('.slidershow__link', '.slidershow__item--activ').attr('href');
-        }
-        if ($this.hasClass('controls__btn_prev')) {
-            existedItem = activeItem.prev();
-            existedItem.addClass('slidershow__item--activ');
-            existedItem.siblings().removeClass('slidershow__item--activ');
-            prevPath = $('.slidershow__link', '.slidershow__item--activ').attr('href');
-
-            browsingReview.fadeOut(function () {
-                preloader.show();
-                browsingReview.attr('src', prevPath).on('load', function () {
-                    browsingLink.attr('href', prevPath);
-                    browsingReview.fadeIn();
-                    preloader.hide();
-                })
-            })
-            prevIndex = existedItem.index();
-        }
-        if (prevIndex == -1) {
-            edgeItem = items.last();
-            edgeItem.addClass('slidershow__item--activ');
-            edgeItem.siblings().removeClass('slidershow__item--activ');
-            prevPath = $('.slidershow__link', '.slidershow__item--activ').attr('href');
-        }
-
-    })
-
-    //click on pagination
-
-    $('.slidershow__link').on('click touchstart', function (event) {
-        event.preventDefault();
-
-        var $this = $(event.currentTarget),
-            container = $this.closest('.slidershow'),
-            targetItem = $this.closest('.slidershow__item'),
-            path = $this.attr('href'),
-            browsingReview = container.find('.slidershow__review'),
-            browsingLink = container.find('.browsing__link'),
-            preloader = $('.slidershow__preloader', container);
-
-        targetItem.addClass('slidershow__item--activ');
-        targetItem.siblings().removeClass('slidershow__item--activ');
+    if ($this.hasClass('controls__btn_next')) {
+        existedItem = activeItem.next();
+        existedItem.addClass('slidershow__item--activ');
+        existedItem.siblings().removeClass('slidershow__item--activ');
+        nextPath = $('.slidershow__link', '.slidershow__item--activ').attr('href');
 
         browsingReview.fadeOut(function () {
             preloader.show();
-            browsingReview.attr('src', path).on('load', function () {
-                browsingLink.attr('href', path);
+            browsingReview.attr('src', nextPath).on('load', function () {
+                browsingLink.attr('href', nextPath);
                 browsingReview.fadeIn();
                 preloader.hide();
             })
         })
+        var nextIndex = existedItem.index();
+    }
+    if (nextIndex == -1) {
+        edgeItem = items.first();
+        edgeItem.addClass('slidershow__item--activ');
+        edgeItem.siblings().removeClass('slidershow__item--activ');
+        nextPath = $('.slidershow__link', '.slidershow__item--activ').attr('href');
+    }
+    if ($this.hasClass('controls__btn_prev')) {
+        existedItem = activeItem.prev();
+        existedItem.addClass('slidershow__item--activ');
+        existedItem.siblings().removeClass('slidershow__item--activ');
+        prevPath = $('.slidershow__link', '.slidershow__item--activ').attr('href');
+
+        browsingReview.fadeOut(function () {
+            preloader.show();
+            browsingReview.attr('src', prevPath).on('load', function () {
+                browsingLink.attr('href', prevPath);
+                browsingReview.fadeIn();
+                preloader.hide();
+            })
+        })
+        prevIndex = existedItem.index();
+    }
+    if (prevIndex == -1) {
+        edgeItem = items.last();
+        edgeItem.addClass('slidershow__item--activ');
+        edgeItem.siblings().removeClass('slidershow__item--activ');
+        prevPath = $('.slidershow__link', '.slidershow__item--activ').attr('href');
+    }
+
+})
+
+//click on pagination
+
+$('.slidershow__link').on('click touchstart', function (event) {
+    event.preventDefault();
+
+    var $this = $(event.currentTarget),
+        container = $this.closest('.slidershow'),
+        targetItem = $this.closest('.slidershow__item'),
+        path = $this.attr('href'),
+        browsingReview = container.find('.slidershow__review'),
+        browsingLink = container.find('.browsing__link'),
+        preloader = $('.slidershow__preloader', container);
+
+    targetItem.addClass('slidershow__item--activ');
+    targetItem.siblings().removeClass('slidershow__item--activ');
+
+    browsingReview.fadeOut(function () {
+        preloader.show();
+        browsingReview.attr('src', path).on('load', function () {
+            browsingLink.attr('href', path);
+            browsingReview.fadeIn();
+            preloader.hide();
+        })
     })
+})
 
 
-    //gallery
+//gallery
 
-    $('[data-fancybox]').fancybox({
-        toolbar: true
-    });
+$('[data-fancybox]').fancybox({
+    toolbar: true
+});
+
+//owl-carousel
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    dots: true,
+    margin: 5,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    responsiveClass:true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        800: {
+            items: 3
+        },
+        1800: {
+            items: 3
+        }
+    }
+})
+
+$('.owl-btn-next').click(function (event) {
+    event.preventDefault();
+    $('.owl-carousel').trigger('next.owl.carousel');
+})
+
+$('.owl-btn-prev').click(function (event) {
+    event.preventDefault();
+    $('.owl-carousel').trigger('prev.owl.carousel');
+})
+
+//Search form
+$('.search__btn').on('click touchstart', function(){
+
+    $(".search__btn").toggleClass("serch-form--close");
+    $(".serch__input").toggleClass("serch__input--active");
+    $(".serch-form").toggleClass("serch-form--active");
+    
+    if ($('.search__btn').hasClass('serch-form--close')) {
+      $('.serch__input').focus();
+    } else {
+      $('.serch__input').blur();
+    }
+
+})
+
+//feedback form
+
+    $('#message-subject').on('click touchstart', function(event){
+        
+          $('#message-subject').focus();
+        
+    })
+    $('#user-name').on('click touchstart', function(event){
+        
+          $('#user-name').focus();
+        
+    })
+    $('#user-email').on('click touchstart', function(event){
+        
+          $('#user-email').focus();
+        
+    })
+    $('#user-message').on('click touchstart', function(event){
+        
+          $('#user-message').focus();
+        
+    })
 
 });
